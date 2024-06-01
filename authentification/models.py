e54dbf37d63d4310befd9798bef4b2105665fcf4
Mbaigolmem Dang-Dang Thiery <thierrymbaye@gmail.com>
2024-06-01 09:14:46 +0100

    Ajoute des models dans l'app d'auth, ajout de forms dans l'app auth, de vue login_page et url pour la vue

--- authentification/models.py
+++ authentification/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+class User(AbstractUser):
+    CREATOR = 'CREATOR'
+    SUBCRIBER = 'SUBCRIBER'
+
+    ROLE_CHOICES = (
+        (CREATOR, 'Createur'),
+        (SUBCRIBER, 'Abonne')
+    )
+
+    profile_phot0 = models.ImageField()
+    role = models.CharField(max_length=30, choices=ROLE_CHOICES)
--- authentification/views.py
+++ authentification/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from forms import LoginForm
+from django.contrib.auth import authenticate, login
 
 # Create your views here.
+def login_page(request):
+    form = LoginForm()
+    message  = ''
+
+    if request.POST:
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            user = authenticate(username = form.cleaned_data['username'], password = form.cleaned_data['password'])
+            if user is not None:
+                login(request, user)
+                message = f"Bonjour, {user.username} ! Vous etes connecte !"
+            else:
+                message = "Identifiant invalide !"
+
+    return render(request, 'authentification/login.html', {
+        'form': form,
+        'message': message
+    })
